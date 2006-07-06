efb432b073f4d9e1ef9717083469bb97ee44b44b
jdsksy <jdsksy@cb113865-1e22-0410-9cd3-9b64e74467e9>
2006-07-06 13:55:32 +0000

    jd_060707
    git-svn-id: http://svn.sourceforge.jp/svnroot/jd4linux/jd/trunk@64 cb113865-1e22-0410-9cd3-9b64e74467e9

--- src/jdversion.h
+++ src/jdversion.h
@@ -4,8 +4,8 @@
 #define _JDVER_H
 
 #define JDCOPYRIGHT "(c) 2006 JD project"
-#define JDVERSIONSTR "1.51b.060628"
-#define JDVERSION 151060628
+#define JDVERSIONSTR "1.51b.060707"
+#define JDVERSION 151060707
 #define JDURL "http://jd4linux.sourceforge.jp/"
 #define JDBBS "http://jd4linux.sourceforge.jp/cgi-bin/bbs/support/"
 
