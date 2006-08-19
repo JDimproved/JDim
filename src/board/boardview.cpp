debfdb9b9a865761ef61abb447616778ebe22208
jdsksy <jdsksy@cb113865-1e22-0410-9cd3-9b64e74467e9>
2006-08-19 17:14:48 +0000

    fix minor bug
    git-svn-id: http://svn.sourceforge.jp/svnroot/jd4linux/jd/trunk@147 cb113865-1e22-0410-9cd3-9b64e74467e9

--- src/board/boardview.cpp
+++ src/board/boardview.cpp
@@ -165,7 +165,7 @@ BoardView::BoardView( const std::string& url,const std::string& arg1, const std:
             break;
 
         case COL_SPEED:
-            width = SESSION::col_speed();
+            width = 1;//SESSION::col_speed(); ぴったり収まるように調整する
             break;
         }
 
