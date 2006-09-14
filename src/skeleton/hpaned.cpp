1862a3e38144a4fc5dfae510f8de051f2d9c4137
jdsksy <jdsksy@cb113865-1e22-0410-9cd3-9b64e74467e9>
2006-09-14 15:51:59 +0000

    show debug message
    git-svn-id: http://svn.sourceforge.jp/svnroot/jd4linux/jd/trunk@217 cb113865-1e22-0410-9cd3-9b64e74467e9

--- src/skeleton/hpaned.cpp
+++ src/skeleton/hpaned.cpp
@@ -1,6 +1,6 @@
 // ライセンス: 最新のGPL
 
-//#define _DEBUG
+#define _DEBUG
 #include "jddebug.h"
 
 #include "hpaned.h"
@@ -44,6 +44,7 @@ bool JDHPaned::on_button_press_event( GdkEventButton* event )
 {
 #ifdef _DEBUG
     std::cout << "JDHPaned::on_button_press_event\n";
+    std::cout << "x = " << event->x << " pos = " << m_pos << std::endl;
 #endif
 
     if( event->type == GDK_BUTTON_PRESS && event->button == 1 ) m_clicked = true;
@@ -55,20 +56,29 @@ bool JDHPaned::on_button_press_event( GdkEventButton* event )
 bool JDHPaned::on_button_release_event( GdkEventButton* event )
 {
 #ifdef _DEBUG
-    std::cout << "JDHPaned::on_butoon_release_event\n";
+    std::cout << "JDHPaned::on_butoon_release_event clicked = " << m_clicked << std::endl;
 #endif
 
     bool ret = Gtk::HPaned::on_button_release_event( event );
 
     // 仕切りをクリックしたら折りたたむ
     if( m_clicked && ! m_drag ){
+
         int pos = Gtk::HPaned::get_position();
 
         // 復元
-        if( pos == 0 ) Gtk::HPaned::set_position( get_position() );
+        if( pos == 0 ){
+#ifdef _DEBUG
+            std::cout << "restore!\n";
+#endif
+            Gtk::HPaned::set_position( get_position() );
+        }
 
         // 折りたたみ
         else{
+#ifdef _DEBUG
+            std::cout << "fold!\n";
+#endif
             m_pos = pos;
             Gtk::HPaned::set_position( 0 );
         }
@@ -86,7 +96,7 @@ bool JDHPaned::on_button_release_event( GdkEventButton* event )
 bool JDHPaned::on_motion_notify_event( GdkEventMotion* event )
 {
 #ifdef _DEBUG
-    std::cout << "JDHPaned::on_motion_notify_event\n";
+//    std::cout << "JDHPaned::on_motion_notify_event\n";
 #endif
 
     m_drag = true;
