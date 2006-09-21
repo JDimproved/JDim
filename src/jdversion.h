42f1cf28ec50d93ab57e2759b46ce6b02295491b
jdsksy <jdsksy@cb113865-1e22-0410-9cd3-9b64e74467e9>
2006-09-21 13:37:39 +0000

    update version number
    git-svn-id: http://svn.sourceforge.jp/svnroot/jd4linux/jd/trunk@245 cb113865-1e22-0410-9cd3-9b64e74467e9

--- src/jdversion.h
+++ src/jdversion.h
@@ -4,11 +4,11 @@
 #define _JDVER_H
 
 // cvs 版の時は JDVERSION_CVS をdefineする
-#define JDVERSION_CVS
+//#define JDVERSION_CVS
 
 #define JDCOPYRIGHT "(c) 2006 JD project"
-#define JDVERSIONSTR "1.70b.060914"
-#define JDVERSION 170060914
+#define JDVERSIONSTR "1.7.0-0.1.rc060921"
+#define JDVERSION 170060921
 #define JDURL "http://jd4linux.sourceforge.jp/"
 #define JDBBS "http://jd4linux.sourceforge.jp/cgi-bin/bbs/support/"
 #define JD2CHLOG "http://jd4linux.sourceforge.jp/old2ch/"
