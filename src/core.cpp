ce93ec31331cd53727125fd1aa968d535212095e
jdsksy <jdsksy@cb113865-1e22-0410-9cd3-9b64e74467e9>
2006-10-28 11:15:53 +0000

    remove double quotation when opening url
    git-svn-id: http://svn.sourceforge.jp/svnroot/jd4linux/jd/trunk@323 cb113865-1e22-0410-9cd3-9b64e74467e9

--- src/core.cpp
+++ src/core.cpp
@@ -2006,11 +2006,17 @@ void Core::open_by_browser( const std::string& url )
     std::string command_openurl = CONFIG::get_command_openurl();
     if( !command_openurl.empty() ){
 
-        command_openurl = MISC::replace_str( command_openurl, "%LINK", url );
-        command_openurl = MISC::replace_str( command_openurl, "%s", url );
+        std::string tmp_url = url;
+
+        // urlの先頭と最後のの " を取る
+        while( *tmp_url.c_str() == '\"' ) tmp_url = tmp_url.substr( 1 );
+        while( tmp_url.c_str()[ tmp_url.length()-1 ] == '\"' ) tmp_url = tmp_url.substr( 0, tmp_url.length()-1 );
+
+        command_openurl = MISC::replace_str( command_openurl, "%LINK", tmp_url );
+        command_openurl = MISC::replace_str( command_openurl, "%s", tmp_url );
 
 #ifdef _DEBUG
-        std::cout << "spawn url = " << url << " command = " << command_openurl << std::endl;
+        std::cout << "spawn url = " << tmp_url << " command = " << command_openurl << std::endl;
 #endif
         Glib::spawn_command_line_async( command_openurl );
     }
