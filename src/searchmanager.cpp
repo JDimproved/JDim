6dfa20bb4b6def3d72ea96cc6ac834ca4c7a096a
jdsksy <jdsksy@cb113865-1e22-0410-9cd3-9b64e74467e9>
2007-01-06 13:54:03 +0000

    added searchmanager
    git-svn-id: http://svn.sourceforge.jp/svnroot/jd4linux/jd/trunk@532 cb113865-1e22-0410-9cd3-9b64e74467e9

---
+++ src/searchmanager.cpp
@@ -0,0 +1,112 @@
+// ライセンス: GPL2
+
+//#define _DEBUG
+#include "jddebug.h"
+
+#include "searchmanager.h"
+
+#include "jdlib/miscmsg.h"
+
+#include "dbtree/interface.h"
+
+typedef void* ( *FUNC )( void * );
+
+CORE::Search_Manager* instance_search_manager = NULL;
+
+CORE::Search_Manager* CORE::get_search_manager()
+{
+    if( ! instance_search_manager ) instance_search_manager = new Search_Manager();
+    assert( instance_search_manager );
+
+    return instance_search_manager;
+}
+
+
+void CORE::delete_search_manager()
+{
+    if( instance_search_manager ) delete instance_search_manager;
+    instance_search_manager = NULL;
+}
+
+///////////////////////////////////////////////
+
+using namespace CORE;
+
+Search_Manager::Search_Manager()
+    : m_searching( false )
+{
+    m_disp.connect( sigc::mem_fun( *this, &Search_Manager::search_fin ) );
+}
+
+Search_Manager::~Search_Manager()
+{}
+
+
+bool Search_Manager::search( const std::string& id,
+                             const std::string& url, const std::string& query,
+                             bool mode_or, bool searchall )
+{
+#ifdef _DEBUG
+    std::cout << "Search_Manager::search " << query << std::endl;
+#endif
+
+    if( m_searching ) return false;
+
+    m_id = id;
+    m_url = url;
+    m_query = query;
+    m_mode_or = mode_or;
+    m_searchall = searchall;
+
+    // 全ログ検索のとき、スレッドを起動する前にメインスレッドで板情報ファイルを
+    // 読み込んでおかないと大量の warning が出る
+    if( m_searchall ) DBTREE::read_boardinfo_all();
+
+    int status;
+    if( ( status = pthread_create( &m_thread, NULL,  ( FUNC ) launcher, ( void * ) this ) )){
+        MISC::ERRMSG( "Search_Manager::search : could not start thread" );
+    }
+    else{
+        m_searching = true;
+        pthread_detach( m_thread );
+    }
+
+    return true;
+}
+
+
+//
+// スレッドのランチャ (static)
+//
+void* Search_Manager::launcher( void* dat )
+{
+    Search_Manager* sm = ( Search_Manager * ) dat;
+    sm->thread_search();
+    return 0;
+}
+
+
+//
+// 検索実行スレッド
+//
+void Search_Manager::thread_search()
+{
+#ifdef _DEBUG
+    std::cout << "Search_Manager::thread_search\n";
+#endif
+
+    if( m_searchall ) m_urllist = DBTREE::search_cache_all( m_url, m_query, m_mode_or );
+    else m_urllist = DBTREE::search_cache( m_url, m_query, m_mode_or );
+
+    m_disp.emit();
+}
+
+
+//
+// 検索終了
+//
+void Search_Manager::search_fin()
+{
+    m_sig_search_fin.emit();
+    m_searching = false;
+}
---
+++ src/searchmanager.h
@@ -0,0 +1,66 @@
+// ライセンス: GPL2
+
+//
+// ログ検索クラス
+//
+
+#ifndef _SEARCHMANAGER_H
+#define _SEARCHMANAGER_H
+
+#include <gtkmm.h>
+#include <pthread.h>
+
+#include <string>
+#include <list>
+
+namespace CORE
+{
+    class Search_Manager
+    {
+        typedef sigc::signal< void > SIG_SEARCH_FIN;
+
+        SIG_SEARCH_FIN m_sig_search_fin;
+
+        pthread_t m_thread;
+        Glib::Dispatcher m_disp;
+
+        std::string m_id;
+        std::string m_url;
+        std::string m_query;
+        bool m_mode_or;
+        bool m_searchall;
+
+        std::list< std::string > m_urllist;
+
+        // 検索実行中
+        bool m_searching;
+
+      public:
+
+        Search_Manager();
+        virtual ~Search_Manager();
+
+        SIG_SEARCH_FIN sig_search_fin(){ return m_sig_search_fin; }
+
+        const bool is_searching() const { return m_searching; }
+        const std::string& get_id() const { return m_id; }
+        const std::list< std::string >& get_urllist() const { return m_urllist; }
+
+        bool search( const std::string& id, const std::string& url, const std::string& query,
+                     bool mode_or, bool searchall );
+
+      private:
+        static void* launcher( void* );
+        void thread_search();
+        void search_fin();
+    };
+
+    ///////////////////////////////////////
+    // インターフェース
+
+    Search_Manager* get_search_manager();
+    void delete_search_manager();
+}
+
+
+#endif
