07999080d7bedb20c99caa330a811c219ee0191a
jdsksy <jdsksy@cb113865-1e22-0410-9cd3-9b64e74467e9>
2007-01-13 07:29:50 +0000

    updated ImgToggleButton
    git-svn-id: http://svn.sourceforge.jp/svnroot/jd4linux/jd/trunk@549 cb113865-1e22-0410-9cd3-9b64e74467e9

--- src/skeleton/imgbutton.h
+++ src/skeleton/imgbutton.h
@@ -12,7 +12,7 @@
 
 namespace SKELETON
 {
-
+    // 普通のボタン
     class ImgButton : public Gtk::Button
     {
         Gtk::Image* m_img;
@@ -38,26 +38,40 @@ namespace SKELETON
     };
 
 
+    // トグルボタン
     class ImgToggleButton : public Gtk::ToggleButton
     {
         Gtk::Image* m_img;
+        Gtk::Label m_label;
+        Gtk::HBox m_hbox;
 
-      public:
+        void set( const std::string& label )
+        {
+            if( ! m_img ) return;
 
-        ImgToggleButton(){}
+            if( label.empty() ) add( *m_img );
+            else {
+                m_label.set_text( label );
+                m_hbox.pack_start( *m_img );
+                m_hbox.pack_start( m_label, Gtk::PACK_SHRINK, 2 );
+                add( m_hbox );
+            }
 
-        void set_img( const Gtk::StockID& stock_id ){
-
-            m_img = Gtk::manage( new Gtk::Image( stock_id, Gtk::ICON_SIZE_MENU ) );
-            add( *m_img );
             set_focus_on_click( false );
         }
 
-        void set_icon( const int id ){
+      public:
+
+        ImgToggleButton( const int id, const std::string label = std::string() ){
 
             m_img = Gtk::manage( new Gtk::Image( ICON::get_icon( id ) ) );
-            add( *m_img );
-            set_focus_on_click( false );
+            set( label );
+        }
+
+        ImgToggleButton( const Gtk::StockID& stock_id, const std::string label = std::string() ){
+
+            m_img = Gtk::manage( new Gtk::Image( stock_id, Gtk::ICON_SIZE_MENU ) );
+            set( label );
         }
     };
 
