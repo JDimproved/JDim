911ad419ec7e8601e582d5f976d675ef2a43b0e2
jdsksy <jdsksy@cb113865-1e22-0410-9cd3-9b64e74467e9>
2007-02-03 15:40:26 +0000

    added toggle_maximize
    git-svn-id: http://svn.sourceforge.jp/svnroot/jd4linux/jd/trunk@633 cb113865-1e22-0410-9cd3-9b64e74467e9

--- src/skeleton/vpaned.cpp
+++ src/skeleton/vpaned.cpp
@@ -8,13 +8,32 @@
 using namespace SKELETON;
 
 JDVPaned::JDVPaned()
-    : Gtk::VPaned()
+    : Gtk::VPaned(),
+      m_clicked( false ),
+      m_drag( false )
 {}
 
 
 JDVPaned::~JDVPaned()
 {}
 
+
+int JDVPaned::get_position()
+{
+    if( ! m_pos ) m_pos = Gtk::VPaned::get_position();
+
+#ifdef _DEBUG
+    std::cout << "JDVPaned::get_position " << m_pos << std::endl;
+#endif
+
+    return m_pos;
+}
+
+void JDVPaned::set_position( int position )
+{
+    m_pos = position;
+    Gtk::VPaned::set_position( position );
+}
 
 // unpack = true の時取り除く
 void JDVPaned::add_remove1( bool unpack, Gtk::Widget& child )
@@ -28,4 +47,74 @@ void JDVPaned::add_remove2( bool unpack, Gtk::Widget& child )
 {
     if( unpack ) remove( child );
     else add2( child );
+}
+
+
+// ページ最大化切り替え
+// page = 0 の時は元に戻す
+void JDVPaned::toggle_maximize( int page )
+{
+    int maxpos = property_max_position();
+    int pos = Gtk::VPaned::get_position();
+
+#ifdef _DEBUG
+    std::cout << "JDVPaned::toggle_maximize page = " << page << " current_pos = " << pos
+              << " pos = " << get_position() << " maxpos = " << maxpos << std::endl;
+#endif
+
+    // 復元
+    if( page == 0 && pos != get_position() ) Gtk::VPaned::set_position( get_position() );
+
+    else if( page == 1 && pos < maxpos ){
+
+        Gtk::VPaned::set_position( maxpos );
+    }
+
+    else if( page == 2 && pos > 0 ){
+
+        Gtk::VPaned::set_position( 0 );
+    }
+}
+
+
+
+bool JDVPaned::on_button_press_event( GdkEventButton* event )
+{
+#ifdef _DEBUG
+    std::cout << "JDVPaned::on_button_press_event\n";
+    std::cout << "x = " << event->x << " pos = " << m_pos << std::endl;
+#endif
+
+    m_clicked = true;
+    m_drag = false;
+
+    return Gtk::VPaned::on_button_press_event( event );
+}
+
+
+bool JDVPaned::on_button_release_event( GdkEventButton* event )
+{
+#ifdef _DEBUG
+    std::cout << "JDVPaned::on_butoon_release_event clicked = " << m_clicked << " drag = " << m_drag << std::endl;
+#endif
+
+    // 仕切りをドラッグした場合
+    if( m_clicked && m_drag ) m_pos = Gtk::VPaned::get_position();
+    m_clicked = false;
+    m_drag = false;
+
+    return Gtk::VPaned::on_button_release_event( event );
+}
+
+
+
+bool JDVPaned::on_motion_notify_event( GdkEventMotion* event )
+{
+#ifdef _DEBUG
+//    std::cout << "JDVPaned::on_motion_notify_event\n";
+#endif
+
+    m_drag = true;
+
+    return Gtk::VPaned::on_motion_notify_event( event );
 }
--- src/skeleton/vpaned.h
+++ src/skeleton/vpaned.h
@@ -12,14 +12,30 @@ namespace SKELETON
 {
     class JDVPaned : public Gtk::VPaned
     {
+        bool m_clicked;
+        bool m_drag;
+        int m_pos;
+
       public:
 
         JDVPaned();
         ~JDVPaned();
 
+        int get_position();
+        void set_position( int position );
+
         // unpack = true の時取り除く
         void add_remove1( bool unpack, Gtk::Widget& child );
         void add_remove2( bool unpack, Gtk::Widget& child );
+
+        // ページ最大化切り替え
+        // page = 0 の時は元に戻す
+        void toggle_maximize( int page );
+
+      protected:
+        virtual bool on_button_press_event( GdkEventButton* event );
+        virtual bool on_button_release_event( GdkEventButton* event );
+        virtual bool on_motion_notify_event( GdkEventMotion* event );
     };
 }
 
