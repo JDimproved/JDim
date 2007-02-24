4c2cb42a7216815f29fb2f163e14175af57f9069
jdsksy <jdsksy@cb113865-1e22-0410-9cd3-9b64e74467e9>
2007-02-24 05:31:43 +0000

    added run
    git-svn-id: http://svn.sourceforge.jp/svnroot/jd4linux/jd/trunk@720 cb113865-1e22-0410-9cd3-9b64e74467e9

--- src/skeleton/msgdiag.h
+++ src/skeleton/msgdiag.h
@@ -24,15 +24,32 @@ namespace SKELETON
         : Gtk::MessageDialog( parent, message, use_markup, type, buttons, modal )
         {}
 
-        MsgDiag( const Glib::ustring& message,
+        // parent がポインタの時は  NULL かどうかで場合分け
+        MsgDiag( Gtk::Window* parent,
+                 const Glib::ustring& message,
                  bool use_markup = false,
                  Gtk::MessageType type = Gtk::MESSAGE_INFO,
                  Gtk::ButtonsType buttons = Gtk::BUTTONS_OK,
                  bool modal = false)
-        : Gtk::MessageDialog( *CORE::get_mainwindow(), message, use_markup, type, buttons, modal )
-        {}
+        : Gtk::MessageDialog( message, use_markup, type, buttons, modal )
+        {
+            if( parent ) set_transient_for( *parent );
+            else set_transient_for( *CORE::get_mainwindow() );
+        }
 
         virtual ~MsgDiag(){}
+
+        virtual int run(){
+
+            // run() 前後でフォーカスが外れて画像ウィンドウの開け閉めをしないように
+            // Image admin にコマンドを送る
+            CORE::core_set_command( "disable_fold_image_win" );
+            int ret = Gtk::MessageDialog::run();
+            CORE::core_set_command( "enable_fold_image_win" );
+
+            return ret;
+        }
+
     };
 }
 
