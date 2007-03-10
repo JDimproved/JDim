b35dc0abf77d5b8fed1faa97ddfc97d8eabea2cc
jdsksy <jdsksy@cb113865-1e22-0410-9cd3-9b64e74467e9>
2007-03-10 08:41:37 +0000

    added defaultconf.h
    git-svn-id: http://svn.sourceforge.jp/svnroot/jd4linux/jd/trunk@769 cb113865-1e22-0410-9cd3-9b64e74467e9

---
+++ src/config/defaultconf.h
@@ -0,0 +1,16 @@
+// ライセンス: GPL2
+//
+// 設定のデフォルト値
+//
+
+#ifndef _DEFAULTCONF_H
+#define _DEFAULTCONF_H
+
+namespace CONFIG
+{
+    enum{
+        MARGIN_POPUP = 30 // レスアンカーとポップアップの間のマージン
+    };
+}
+
+#endif
