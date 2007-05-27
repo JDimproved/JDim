28195407596a73bacb2a5c06f07fd949b9f5dcfb
jdsksy <jdsksy@cb113865-1e22-0410-9cd3-9b64e74467e9>
2007-05-27 13:31:06 +0000

    changed URL of manual page
    git-svn-id: http://svn.sourceforge.jp/svnroot/jd4linux/jd/trunk@1072 cb113865-1e22-0410-9cd3-9b64e74467e9

--- src/jdversion.h
+++ src/jdversion.h
@@ -3,14 +3,21 @@
 #ifndef _JDVER_H
 #define _JDVER_H
 
+// ベータ版の場合は define する
+#define JDVERSION_BETA
+
+#ifdef JDVERSION_BETA
 // svn 版の時は JDVERSION_SVN をdefineする
 #define JDVERSION_SVN
+#define JDHELP "http://jd4linux.sourceforge.jp/manual/develop/"
+#else
+#define JDHELP "http://jd4linux.sourceforge.jp/manual/"
+#endif
 
 #define JDCOPYRIGHT "(c) 2006-2007 JD project"
 #define JDVERSIONSTR "1.9.5-beta070516"
 #define JDVERSION 195070516
 #define JDURL "http://jd4linux.sourceforge.jp/"
-#define JDHELP "http://jd4linux.sourceforge.jp/help/help.html"
 #define JDBBS "http://jd4linux.sourceforge.jp/cgi-bin/bbs/support/"
 #define JD2CHLOG "http://jd4linux.sourceforge.jp/old2ch/"
 
