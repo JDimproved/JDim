80bfca6eafe2d02d1a1d50eb75243894f54bc44a
jdsksy <jdsksy@cb113865-1e22-0410-9cd3-9b64e74467e9>
2007-11-08 14:19:22 +0000

    Gtk::ICON_SIZE_BUTTON -> Gtk::ICON_SIZE_MENU
    git-svn-id: http://svn.sourceforge.jp/svnroot/jd4linux/jd/trunk@1487 cb113865-1e22-0410-9cd3-9b64e74467e9

--- src/skeleton/imgbutton.h
+++ src/skeleton/imgbutton.h
@@ -82,7 +82,7 @@ namespace SKELETON
 
         ImgToggleButton( const Gtk::StockID& stock_id, const std::string label = std::string() ){
 
-            m_img = Gtk::manage( new Gtk::Image( stock_id, Gtk::ICON_SIZE_BUTTON ) );
+            m_img = Gtk::manage( new Gtk::Image( stock_id, Gtk::ICON_SIZE_MENU ) );
             set( label );
         }
     };
