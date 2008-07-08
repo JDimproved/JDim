781f0168e74c10c5700e691725d315fc7a3bb454
jdsksy <jdsksy@cb113865-1e22-0410-9cd3-9b64e74467e9>
2008-07-08 15:45:48 +0000

    updated browsers.cpp
    git-svn-id: http://svn.sourceforge.jp/svnroot/jd4linux/jd/trunk@2185 cb113865-1e22-0410-9cd3-9b64e74467e9

--- src/browsers.cpp
+++ src/browsers.cpp
@@ -13,8 +13,8 @@ namespace CORE
     char browsers[ BROWSER_NUM ][ 2 ][ MAX_TEXT ]={
 
         { "ユーザ設定", "" },
-        { "RedHat系標準ブラウザ",    "xdg-open \"%LINK\"" },
-        { "firefox 1.5/2.0 以降", "firefox \"%LINK\"" },
+        { "標準ブラウザ",    "xdg-open \"%LINK\"" },
+        { "firefox 1.5/2.0/3.0 以降", "firefox \"%LINK\"" },
         { "firefox 1.0*", "firefox -remote \"openURL(%LINK,new-tab)\"" },
         { "opera 9.*",    "opera -remote \"openURL(%LINK,new-tab)\"" }
     };
