1283eacda61de1dd8ccfe694444f427fe05475dd
jdsksy <jdsksy@cb113865-1e22-0410-9cd3-9b64e74467e9>
2008-11-10 14:11:45 +0000

    load setting.txt and head.txt with basicauth
    git-svn-id: http://svn.sourceforge.jp/svnroot/jd4linux/jd/trunk@2457 cb113865-1e22-0410-9cd3-9b64e74467e9

--- src/dbtree/settingloader.cpp
+++ src/dbtree/settingloader.cpp
@@ -74,7 +74,8 @@ void SettingLoader::create_loaderdata( JDLIB::LOADERDATA& data )
         data.size_buf = CONFIG::get_loader_bufsize();
         data.timeout = CONFIG::get_loader_timeout();
         if( ! get_date_modified().empty() ) data.modified = get_date_modified();
-    }
+        data.basicauth = DBTREE::board_basicauth( m_url_boadbase );
+   }
 }
 
 
