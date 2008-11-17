dda4a740a5f504c8bbf2d18ee6b87ddf26719a38
jdsksy <jdsksy@cb113865-1e22-0410-9cd3-9b64e74467e9>
2008-11-17 14:01:59 +0000

    version up : jd-2.0.3-rc081117
    git-svn-id: http://svn.sourceforge.jp/svnroot/jd4linux/jd/trunk@2475 cb113865-1e22-0410-9cd3-9b64e74467e9

--- src/jdversion.h
+++ src/jdversion.h
@@ -11,13 +11,13 @@
 #endif
 
 // svn 版の時は JDVERSION_SVN をdefineする
-#define JDVERSION_SVN
+//#define JDVERSION_SVN
 
 #define MAJORVERSION 2
 #define MINORVERSION 0
 #define MICROVERSION 3
-#define JDDATE    "081110"
-#define JDTAG     "beta"
+#define JDDATE    "081117"
+#define JDTAG     "rc"
 
 //---------------------------------
 
