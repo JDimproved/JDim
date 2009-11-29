1c258dfd47a41755f3f18ee101f86df4920b91c3
jdsksy <jdsksy@cb113865-1e22-0410-9cd3-9b64e74467e9>
2009-11-29 13:43:15 +0000

    version up : 2.5.0-rc091129
    git-svn-id: http://svn.sourceforge.jp/svnroot/jd4linux/jd/trunk@3194 cb113865-1e22-0410-9cd3-9b64e74467e9

--- jd.spec
+++ jd.spec
@@ -10,7 +10,7 @@
 # Defined by upsteam
 #
 %define         main_ver      2.5.0
-%define         strtag        beta091123
+%define         strtag        rc091129
 %define         repoid        ?????
 # Define this if this is pre-version
 %define         pre_release   1
--- src/jdversion.h
+++ src/jdversion.h
@@ -11,13 +11,13 @@
 #endif
 
 // svn 版の時は JDVERSION_SVN をdefineする
-#define JDVERSION_SVN
+//#define JDVERSION_SVN
 
 #define MAJORVERSION 2
 #define MINORVERSION 5
 #define MICROVERSION 0
-#define JDDATE    "091123"
-#define JDTAG     "beta"
+#define JDDATE    "091129"
+#define JDTAG     "rc"
 
 //---------------------------------
 
