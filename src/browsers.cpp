f98c4d374136cee47c7e0773ac046535656e6920
jdsksy <jdsksy@cb113865-1e22-0410-9cd3-9b64e74467e9>
2010-02-20 05:37:37 +0000

    added chrome into browserspref
    git-svn-id: http://svn.sourceforge.jp/svnroot/jd4linux/jd/trunk@3370 cb113865-1e22-0410-9cd3-9b64e74467e9

--- src/browsers.cpp
+++ src/browsers.cpp
@@ -5,7 +5,7 @@
 enum
 {
     MAX_TEXT = 256,
-    BROWSER_NUM = 5
+    BROWSER_NUM = 6
 };
 
 namespace CORE
@@ -17,12 +17,14 @@ namespace CORE
         { "ie",    "C:/Program\\ Files/Internet\\ Explorer/iexplore.exe \"%LINK\"" },
         { "firefox 1.5/2.0/3.0 以降", "C:/Program\\ Files/Mozilla\\ Firefox/firefox.exe \"%LINK\"" },
         { "firefox 1.0*", "C:/Program\\ Files/Mozilla\\ Firefox/firefox.exe -remote \"openURL(%LINK,new-tab)\"" },
-        { "opera 9.*",    "C:/Program\\ Files/Opera/opera.exe \"%LINK\"" }
+        { "opera 9.*",    "C:/Program\\ Files/Opera/opera.exe \"%LINK\"" },
+        { "chrome",    "" }
 #else
         { "標準ブラウザ(xdg-open)",    "xdg-open \"%LINK\"" },
         { "firefox 1.5/2.0/3.0 以降", "firefox \"%LINK\"" },
         { "firefox 1.0*", "firefox -remote \"openURL(%LINK,new-tab)\"" },
-        { "opera 9.*",    "opera -remote \"openURL(%LINK,new-tab)\"" }
+        { "opera 9.*",    "opera -remote \"openURL(%LINK,new-tab)\"" },
+        { "chrome",    "google-chrome %U -open \"%LINK\"" }
 #endif
     };
 
