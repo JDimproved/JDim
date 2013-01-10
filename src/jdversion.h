2b7bee71c6ed02afbd21ac1201312942a62fe660
weise <weise@cb113865-1e22-0410-9cd3-9b64e74467e9>
2013-01-10 14:37:19 +0000

    Change to 2013 year
    git-svn-id: http://svn.sourceforge.jp/svnroot/jd4linux/jd/trunk@4056 cb113865-1e22-0410-9cd3-9b64e74467e9

--- src/jdversion.h
+++ src/jdversion.h
@@ -27,7 +27,7 @@
 //---------------------------------
 
 #define JDCOMMENT "JD は gtkmm/GTK+2 を用いた2chブラウザです。"
-#define JDCOPYRIGHT "(c) 2006-2012 JD project"
+#define JDCOPYRIGHT "(c) 2006-2013 JD project"
 #define JDBBS CONFIG::get_url_jdhp()+"cgi-bin/bbs/support/"
 #define JD2CHLOG CONFIG::get_url_jdhp()+"old2ch/"
 #define JDHELP CONFIG::get_url_jdhp()+"manual/"+MISC::itostr( JDVERSION )+"/"
