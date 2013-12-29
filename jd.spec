ea852c72018410a7b756be4ab75f0a554c988788
weise <weise@cb113865-1e22-0410-9cd3-9b64e74467e9>
2013-12-29 23:30:03 +0000

    version up : jd-2.8.7-rc131230
    git-svn-id: http://svn.sourceforge.jp/svnroot/jd4linux/jd/trunk@4136 cb113865-1e22-0410-9cd3-9b64e74467e9

--- jd.spec
+++ jd.spec
@@ -10,7 +10,7 @@
 # Defined by upsteam
 #
 %define         main_ver      2.8.7
-%define         strtag        beta130518
+%define         strtag        rc131230
 %define         repoid        ?????
 # Define this if this is pre-version
 %define         pre_release   1
--- src/jdversion.h
+++ src/jdversion.h
@@ -11,13 +11,13 @@
 #endif
 
 // svn 版の時は JDVERSION_SVN をdefineする
-#define JDVERSION_SVN
+//#define JDVERSION_SVN
 
 #define MAJORVERSION 2
 #define MINORVERSION 8
 #define MICROVERSION 7
-#define JDDATE    "130518"
-#define JDTAG     "beta"
+#define JDDATE    "131230"
+#define JDTAG     "rc"
 
 //---------------------------------
 
