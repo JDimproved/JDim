7c871ab04593941d3cc4cba526e065c2597445e8
weise <weise@cb113865-1e22-0410-9cd3-9b64e74467e9>
2014-04-26 17:03:40 +0000

    change default to find.2ch.net and p2.2ch.sc (ref. 4154)
    git-svn-id: http://svn.sourceforge.jp/svnroot/jd4linux/jd/trunk@4174 cb113865-1e22-0410-9cd3-9b64e74467e9

--- src/config/defaultconf.h
+++ src/config/defaultconf.h
@@ -207,7 +207,7 @@ namespace CONFIG
 #define CONF_LOGIN2CH "https://2chv.tora3.net/futen.cgi"
 
 // p2の認証サーバのアドレス
-#define CONF_LOGINP2 "http://p2.moritapo.jp/p2/"
+#define CONF_LOGINP2 "http://p2.2ch.sc/p2/"
 
 // BEの認証サーバのアドレス
 #define CONF_LOGINBE "http://be.2ch.net/test/login.php"
@@ -216,11 +216,11 @@ namespace CONFIG
 #define CONF_URL_BBSMENU "http://menu.2ch.net/bbsmenu.html"
 
 // スレタイ検索用メニュータイトルアドレス
-#define CONF_MENU_SEARCH_TITLE  "スレタイ検索 (find2ch)"
-#define CONF_URL_SEARCH_TITLE "http://find.moritapo.jp/?STR=$TEXTX&COUNT=50&TYPE=TITLE&BBS=ALL"
+#define CONF_MENU_SEARCH_TITLE  "スレタイ検索 (find.2ch.net)"
+#define CONF_URL_SEARCH_TITLE "http://find.2ch.net/search?q=$TEXTE&board=&match=full&size=50"
 
 // スレタイ検索用正規表現
-#define CONF_REGEX_SEARCH_TITLE "<a href=\"(http[^\"]*)\">(.+?)</a> \\(([0-9]{1,4})\\)"
+#define CONF_REGEX_SEARCH_TITLE "<a [^>]*href=\"(http[^\"]*)\" class=\"title\">([^<]+)</a>"
 
 // WEB検索用メニュータイトルアドレス
 #define CONF_MENU_SEARCH_WEB  "WEB検索 (google)"
@@ -228,8 +228,8 @@ namespace CONFIG
 
 // p2 書き込み用アドレス
 
-#define CONF_URL_WRITEP2 "http://p2.moritapo.jp/p2/post_form.php?host=$HOST&bbs=$BBSNAME&key=$DATNAME"
-#define CONF_URL_RESP2 "http://p2.moritapo.jp/p2/post_form.php?host=$HOST&bbs=$BBSNAME&key=$DATNAME&popup=1&inyou=2&resnum=$NUMBER"
+#define CONF_URL_WRITEP2 "http://p2.2ch.sc/p2/post_form.php?host=$HOST&bbs=$BBSNAME&key=$DATNAME"
+#define CONF_URL_RESP2 "http://p2.2ch.sc/p2/post_form.php?host=$HOST&bbs=$BBSNAME&key=$DATNAME&popup=1&inyou=2&resnum=$NUMBER"
 
 // 2chのクッキー:HAP
 #define CONF_COOKIE_HAP ""
