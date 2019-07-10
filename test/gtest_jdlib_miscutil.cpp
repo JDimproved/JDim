37080e08c6eba5054c45daa6639b2608f2c6e327
Masayuki Yamamoto <ma3yuki.8mamo10@gmail.com>
2019-07-10 22:40:16 +0900

    Add test case for MISC::split_line

---
+++ test/gtest_jdlib_miscutil.cpp
@@ -0,0 +1,51 @@
+// License: GPL2
+
+#include "jdlib/miscutil.h"
+
+#include "gtest/gtest.h"
+
+
+namespace {
+
+class SplitLineTest : public ::testing::Test {};
+
+TEST_F(SplitLineTest, split_empty)
+{
+    std::list< std::string > expect = {};
+    EXPECT_EQ( expect, MISC::split_line( u8"" ) );
+}
+
+TEST_F(SplitLineTest, split_U_0020)
+{
+    std::list< std::string > expect = {};
+    EXPECT_EQ( expect, MISC::split_line( u8"    " ) );
+
+    expect.assign( { u8"the", u8"quick", u8"brown", u8"fox" } );
+    EXPECT_EQ( expect, MISC::split_line( u8" the quick  brown   fox  " ) );
+}
+
+TEST_F(SplitLineTest, split_U_3000)
+{
+    std::list< std::string > expect = {};
+    EXPECT_EQ( expect, MISC::split_line( u8"\u3000 \u3000 " ) );
+
+    expect.assign( { u8"the", u8"quick", u8"brown", u8"fox" } );
+    EXPECT_EQ( expect, MISC::split_line( u8"\u3000the\u3000quick \u3000brown\u3000 \u3000fox\u3000 " ) );
+}
+
+TEST_F(SplitLineTest, split_doublequote_U_0020)
+{
+    std::list< std::string > expect = {};
+    EXPECT_EQ( expect, MISC::split_line( u8"  \"\"  " ) );
+
+    expect.assign( { u8"the quick", u8" ", u8" brown   fox " } );
+    EXPECT_EQ( expect, MISC::split_line( u8" \"the quick\" \" \" \" brown   fox \" " ) );
+}
+
+TEST_F(SplitLineTest, split_doublequote_U_3000)
+{
+    std::list< std::string > expect = { u8"the\u3000quick", u8"\u3000", u8"\u3000brown \u3000fox\u3000" };
+    EXPECT_EQ( expect, MISC::split_line( u8"\u3000\"the\u3000quick\" \"\u3000\" \"\u3000brown \u3000fox\u3000\"" ) );
+}
+
+} // namespace
