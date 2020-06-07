11738b41ca07d92dac29dac048e6f4bbb0c99d7b
Masayuki Yamamoto <ma3yuki.8mamo10@gmail.com>
2020-06-07 13:37:24 +0900

    Implement FrontLoader
    板のフロントページを読み込むするクラスを実装します。
    
    - フロントページはキャッシュに保存しない
    - 読み込んだHTMLは解析関数に渡される
    - 受信したデータが空のときはキーワードの解析をしないようにする
    
    修正にあたり不具合報告をしていただきありがとうございました。
    https://mao.5ch.net/test/read.cgi/linux/1584619744/407

---
+++ src/dbtree/frontloader.h
@@ -0,0 +1,47 @@
+// ライセンス: GPL2
+//
+// 板のフロントページのローダー
+//
+
+#ifndef JDIM_FRONTLOADER_H
+#define JDIM_FRONTLOADER_H
+
+#include "skeleton/textloader.h"
+
+#include <string>
+
+namespace JDLIB
+{
+    class LOADERDATA;
+}
+
+namespace DBTREE
+{
+    class FrontLoader : public SKELETON::TextLoader
+    {
+        std::string m_url_boadbase;
+
+      public:
+
+        FrontLoader( const std::string& url_boardbase );
+        ~FrontLoader() = default;
+
+      protected:
+
+        std::string get_url() override { return m_url_boadbase; }
+        std::string get_path() override { return {}; } // キャッシュには保存しない
+        std::string get_charset() override;
+
+        // ロード用データ作成
+        void create_loaderdata( JDLIB::LOADERDATA& data ) override;
+
+        // ロード後に呼び出される
+        void parse_data() override;
+
+      private:
+
+        void receive_cookies() override;
+    };
+}
+
+#endif // JDIM_FRONTLOADER_H
