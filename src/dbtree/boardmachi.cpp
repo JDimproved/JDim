c63e7eefeed771e8858c7dc4a9c8ab783c958eeb
Masayuki Yamamoto <ma3yuki.8mamo10@gmail.com>
2020-06-14 16:07:19 +0900

    BoardMachi: Unite local variables to avoid shadowing.
    ローカル変数の名前がシャドウイングされているとcppcheckに指摘されたため
    変数を統合します。
    
    cppcheckのレポート
    ```
    src/dbtree/boardmachi.cpp:114:27: style: Local variable 'urldat' shadows outer variable [shadowVariable]
            const std::string urldat = BoardBase::url_dat( MISC::replace_str( url, "read.pl", "read.cgi" ), num_from, num_to, num_str );
                              ^
    src/dbtree/boardmachi.cpp:108:23: note: Shadowed declaration
        const std::string urldat = BoardBase::url_dat( url, num_from, num_to, num_str );
                          ^
    src/dbtree/boardmachi.cpp:114:27: note: Shadow variable
            const std::string urldat = BoardBase::url_dat( MISC::replace_str( url, "read.pl", "read.cgi" ), num_from, num_to, num_str );
                              ^
    ```

--- src/dbtree/boardmachi.cpp
+++ src/dbtree/boardmachi.cpp
@@ -105,13 +105,13 @@ std::string BoardMachi::url_dat( const std::string& url, int& num_from, int& num
     if( empty() ) return std::string();
 
     // read.cgi 型の判定
-    const std::string urldat = BoardBase::url_dat( url, num_from, num_to, num_str );
+    std::string urldat = BoardBase::url_dat( url, num_from, num_to, num_str );
     if( ! urldat.empty() ) return urldat;
 
     // 旧形式(read.pl)型の場合
     if( url.find( "read.pl" ) != std::string::npos ){
 
-        const std::string urldat = BoardBase::url_dat( MISC::replace_str( url, "read.pl", "read.cgi" ), num_from, num_to, num_str );
+        urldat = BoardBase::url_dat( MISC::replace_str( url, "read.pl", "read.cgi" ), num_from, num_to, num_str );
         if( ! urldat.empty() ) return urldat;
     }
 
