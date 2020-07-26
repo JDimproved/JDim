fcdbad301cc7d8ecc0f827bc1fdcde64483dcfac
Masayuki Yamamoto <ma3yuki.8mamo10@gmail.com>
2020-07-26 17:52:21 +0900

    DragableNoteBook: Remove member functions which are no longer in use (#416)
    使われなくなったメンバ関数を削除しGTK2対応コードを整理します。

--- src/skeleton/dragnote.h
+++ src/skeleton/dragnote.h
@@ -115,11 +115,6 @@ namespace SKELETON
 
         void clock_in();
 
-#if !GTKMM_CHECK_VERSION(3,0,0)
-        // 枠描画
-        void draw_box( Gtk::Widget* widget, GdkEventExpose* event );
-#endif
-
         bool get_show_tabs() const { return m_show_tabs; }
         void set_show_tabs( bool show_tabs );
         void set_scrollable( bool scrollable );
@@ -165,16 +160,6 @@ namespace SKELETON
         Gtk::Button& get_tabswitch_button(){ return m_bt_tabswitch.get_button(); }
 
       private:
-
-#if !GTKMM_CHECK_VERSION(3,0,0)
-        bool on_expose_event( GdkEventExpose* event ) override;
-#endif
-
-#if !GTKMM_CHECK_VERSION(3,0,0)
-        // DragableNoteBook を構成している各Notebookの高さ
-        // 及びタブの高さと位置を取得 ( 枠の描画用 )
-        Alloc_NoteBook get_alloc_notebook();
-#endif
 
         // ツールバー取得
         SKELETON::ToolBar* get_toolbar( int page );
