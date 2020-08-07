772095b0792e5ad38ea3da59b28ee6fd47428639
Masayuki Yamamoto <ma3yuki.8mamo10@gmail.com>
2020-08-07 21:26:23 +0900

    Remove compile conditions for value getting/setting part5 (#434)
    GTK2とGTK3で値の取得・設定方法が異なりコンパイル条件で分かれている箇所を
    整理します。

--- src/skeleton/aboutdiag.cpp
+++ src/skeleton/aboutdiag.cpp
@@ -166,11 +166,7 @@ void AboutDiag::set_version( const Glib::ustring& version )
     m_label_version.set_label( version );
 
     // TODO: GTKのcssで設定するか？
-#if GTKMM_CHECK_VERSION(3,0,0)
     Pango::FontDescription font_discription_version = m_label_version.get_style_context()->get_font();
-#else
-    Pango::FontDescription font_discription_version = m_label_version.get_style()->get_font();
-#endif
     const int label_version_font_size = font_discription_version.get_size();
     font_discription_version.set_size( label_version_font_size * 5 / 3 );
     font_discription_version.set_weight( Pango::WEIGHT_BOLD );
