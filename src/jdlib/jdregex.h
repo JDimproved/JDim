eff85206a58e0a269aa3e93ad7758869052544a4
Masayuki Yamamoto <ma3yuki.8mamo10@gmail.com>
2020-11-28 15:43:40 +0900

    Improve JDLIB::Regex class
    - `RegexPattern`クラスを導入してコンパイル済みの正規表現オブジェクトと
      検索実行してマッチ結果を保持する`Regex`クラスに分割します。
    - 正規表現のコンパイルや検索に失敗したときエラー内容をテキストで
      取得できるようにします。
    - 検索実行時に行頭行末にマッチさせないオプションを追加します。
    - マッチ結果を使って文字列を置換するメンバー関数を追加します。
    
    Co-authored-by: JD Project <jd.project@acc574344b8506f1335297eaa7f74be0f7ea992b>

--- src/jdlib/jdregex.h
+++ src/jdlib/jdregex.h
@@ -25,46 +25,85 @@
 
 namespace JDLIB
 {
-    class Regex
+    class Regex;
+
+    class RegexPattern
     {
-        std::vector< int > m_pos;
-        std::vector< std::string > m_results;
+        friend class Regex;
 
 #ifdef POSIX_STYLE_REGEX_API
-        regex_t m_reg;
+        // onigurumaではregexec()の引数regex_t*がconst修飾されていない
+        // そのためRegex::match()のコンパイルエラーを回避するためmutable修飾が必要
+        mutable regex_t m_regex;
 #else
-        GRegex* m_reg{};
+        GRegex* m_regex{};
 #endif
-
         bool m_compiled{};
         bool m_newline{};
         bool m_wchar{};
+#ifdef POSIX_STYLE_REGEX_API
+        int m_error{};
+#else
+        GError *m_error{};
+#endif
+
+    public:
+        RegexPattern() noexcept = default;
+        RegexPattern( const std::string& reg, const bool icase, const bool newline,
+                      const bool usemigemo = false, const bool wchar = false );
+        ~RegexPattern() noexcept;
+
+        // regex_tを複製する方法がないためcopy禁止にする
+        RegexPattern( const RegexPattern& ) = delete;
+        RegexPattern& operator=( const RegexPattern& ) = delete;
+        // moveは許可
+        RegexPattern( RegexPattern&& ) noexcept;
+        RegexPattern& operator=( RegexPattern&& ) noexcept;
+
+        bool set( const std::string& reg, const bool icase, const bool newline,
+                  const bool usemigemo = false, const bool wchar = false );
+        void clear();
+        bool compiled() const noexcept { return m_compiled; }
+        std::string errstr() const;
+    };
+
+
+    class Regex
+    {
+        std::vector<int> m_pos;
+        std::vector<std::string> m_results;
 
         // 全角半角を区別しないときに使う変換用バッファ
         // 処理可能なバッファ長は regoff_t (= int) のサイズに制限される
         std::string m_target_asc;
-        std::vector< int > m_table_pos;
+        std::vector<int> m_table_pos;
 
     public:
 
-        Regex() = default;
-        ~Regex();
+        Regex() noexcept = default;
+        ~Regex() noexcept = default;
 
-        void dispose();
-        
+        // notbol : 行頭マッチは必ず失敗する
+        // noteol : 行末マッチは必ず失敗する
+        bool match( const RegexPattern& creg, const std::string& target, const std::size_t offset,
+                    const bool notbol = false, const bool noteol = false );
+
         // icase : 大文字小文字区別しない
         // newline :  . に改行をマッチさせない
         // usemigemo : migemo使用 (コンパイルオプションで指定する必要あり)
         // wchar : 全角半角の区別をしない
-        bool compile( const std::string& reg, const bool icase, const bool newline, const bool usemigemo,
-                      const bool wchar );
+        bool exec( const std::string& reg, const std::string& target, const std::size_t offset,
+                   const bool icase, const bool newline, const bool usemigemo = false,
+                   const bool wchar = false ) {
+            RegexPattern pattern( reg, icase, newline, usemigemo, wchar );
+            return match( pattern, target, offset );
+        }
 
-        bool exec( const std::string& target, const size_t offset );
-        bool exec( const std::string& reg, const std::string& target, const size_t offset, const bool icase,
-                   const bool newline, const bool usemigemo, const bool wchar );
+        // マッチした文字列と \0〜\9 を置換する
+        std::string replace( const std::string& repstr ) const;
 
-        int pos( const size_t num );
-        std::string str( const size_t num );
+        int pos( std::size_t num ) const noexcept;
+        std::string str( std::size_t num ) const;
     };
 }
 
