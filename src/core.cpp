83183b05df692ccd68fa1efcceef8ecefe82c16e
Masayuki Yamamoto <15698961+ma8ma@users.noreply.github.com>
2022-09-17 22:55:22 +0900

    statusbar: Reset default color if setting turned off
    スレッドの状態が変わったときにステータスバーの色を変える設定が
    オフになっているときはステータスバーをデフォルト色に戻します。

--- src/core.cpp
+++ src/core.cpp
@@ -2979,6 +2979,7 @@ void Core::set_command( const COMMAND_ARGS& command )
 
     else if( command.command  == "set_status_color" ){
         if( CONFIG::get_change_stastatus_color() ) m_win_main.set_status_color( command.arg1 );
+        else m_win_main.set_status_color( "" );
     }
 
     // 一時的にステータスバーの表示を変える( マウスオーバーでのURL表示用 )
