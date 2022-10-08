5b7b433e84fb01a8d7670beec286eb955e01ae38
Masayuki Yamamoto <ma3yuki.8mamo10@gmail.com>
2022-10-08 21:25:32 +0900

    Add test cases for MISC::norm()

--- test/gtest_jdlib_miscutil.cpp
+++ test/gtest_jdlib_miscutil.cpp
@@ -4,6 +4,8 @@
 
 #include "gtest/gtest.h"
 
+#include <numeric> // std::iota()
+
 
 namespace {
 
@@ -969,6 +971,399 @@ TEST_F(MISC_AscTest, fullwidth_hiragana_combining_voiced_sound_mark)
     // 合成済み文字の有無に関係なく(半)濁点の結合文字が付いている全角平仮名は変換されない
     // NOTE: 組み合わせが多いのでテストは網羅していない
     MISC::asc( fullwidth, output, table );
+
+    EXPECT_EQ( fullwidth, output );
+    EXPECT_EQ( output.size(), table.size() - 1 );
+    for( int i = 0, size = table.size(); i < size; ++i ) {
+        EXPECT_EQ( i, table.at( i ) );
+    }
+}
+
+
+class MISC_NormTest : public ::testing::Test {};
+
+TEST_F(MISC_NormTest, empty_input)
+{
+    std::string output;
+    std::vector<int> table;
+
+    // 入力はヌル終端文字列
+    MISC::norm( "", output, &table );
+
+    EXPECT_EQ( "", output );
+    EXPECT_EQ( 0, output.size() );
+    // 文字列の終端（ヌル文字）の位置が追加されるためtableのサイズが+1大きくなる
+    EXPECT_EQ( 1, table.size() );
+    EXPECT_EQ( 0, table.at( 0 ) );
+}
+
+TEST_F(MISC_NormTest, halfwidth_latin_capital_letter)
+{
+    std::string output;
+    std::vector<int> table;
+
+    MISC::norm( "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.", output, &table );
+
+    EXPECT_EQ( "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.", output );
+    EXPECT_EQ( output.size(), table.size() - 1 );
+    for( int i = 0, size = table.size(); i < size; ++i ) {
+        EXPECT_EQ( i, table.at( i ) );
+    }
+}
+
+TEST_F(MISC_NormTest, halfwidth_latin_small_letter)
+{
+    std::string output;
+    std::vector<int> table;
+
+    MISC::norm( "the quick brown fox jumps over the lazy dog.", output, &table );
+
+    EXPECT_EQ( "the quick brown fox jumps over the lazy dog.", output );
+    EXPECT_EQ( output.size(), table.size() - 1 );
+    for( int i = 0, size = table.size(); i < size; ++i ) {
+        EXPECT_EQ( i, table.at( i ) );
+    }
+}
+
+TEST_F(MISC_NormTest, halfwidth_digit_sign)
+{
+    std::string output;
+    std::vector<int> table;
+
+    MISC::norm( "1234567890+-*/", output, &table );
+
+    EXPECT_EQ( "1234567890+-*/", output );
+    EXPECT_EQ( output.size(), table.size() - 1 );
+    for( int i = 0, size = table.size(); i < size; ++i ) {
+        EXPECT_EQ( i, table.at( i ) );
+    }
+}
+
+TEST_F(MISC_NormTest, halfwidth_append_data)
+{
+    std::string output = "123";
+    std::vector<int> table = { 0, 1, 2 };
+
+    // アウトプット引数は初期化せずデータを追加する
+    MISC::norm( "hello", output, &table );
+
+    EXPECT_EQ( "123hello", output );
+    EXPECT_EQ( output.size(), table.size() - 1 );
+    const std::vector<int> expected_table = { 0, 1, 2, 0, 1, 2, 3, 4, 5 };
+    EXPECT_EQ( expected_table, table );
+}
+
+
+std::vector<int> norm_expected_table_fullwidth_quick_brown_fox()
+{
+    // 全角英数字から半角英数字に変換したときの文字列の位置を保存しておくテーブルのテストデータ
+    return {
+        // TH E  _  Q   U   I   C   K   _0  B   R   O   W   N   _   F   O   X   _
+        0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57,
+        // JU   M   P   S   _   O   V   E   R   _   T   H   E   _    L    A    Z    Y
+        60, 63, 66, 69, 72, 75, 78, 81, 84, 87, 90, 93, 96, 99, 102, 105, 108, 111, 114,
+        // _ D    O    G    .    U+0000
+        117, 120, 123, 126, 129, 132,
+    };
+}
+
+TEST_F(MISC_NormTest, fullwidth_latin_capital_letter)
+{
+    std::string output;
+    std::vector<int> table;
+
+    MISC::norm( "ＴＨＥ　ＱＵＩＣＫ　ＢＲＯＷＮ　ＦＯＸ　ＪＵＭＰＳ　ＯＶＥＲ　ＴＨＥ　ＬＡＺＹ　ＤＯＧ．", output,
+                &table );
+
+    // 和字間隔(U+3000)と全角ピリオド(U+FF0E)も半角スペースに変換される
+    EXPECT_EQ( "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.", output );
+    EXPECT_EQ( output.size(), table.size() - 1 );
+    EXPECT_EQ( norm_expected_table_fullwidth_quick_brown_fox(), table );
+}
+
+TEST_F(MISC_NormTest, fullwidth_latin_small_letter)
+{
+    std::string output;
+    std::vector<int> table;
+
+    MISC::norm( "ｔｈｅ　ｑｕｉｃｋ　ｂｒｏｗｎ　ｆｏｘ　ｊｕｍｐｓ　ｏｖｅｒ　ｔｈｅ　ｌａｚｙ　ｄｏｇ．", output,
+                &table );
+
+    EXPECT_EQ( "the quick brown fox jumps over the lazy dog.", output );
+    EXPECT_EQ( output.size(), table.size() - 1 );
+    EXPECT_EQ( norm_expected_table_fullwidth_quick_brown_fox(), table );
+}
+
+TEST_F(MISC_NormTest, fullwidth_digit_sign)
+{
+    std::string output;
+    std::vector<int> table;
+
+    MISC::norm( "１２３４５６７８９０＋−＊／", output, &table );
+
+    // 全角Minus Sign(U+2212)以外は半角に変換される
+    EXPECT_EQ( "1234567890+−*/", output );
+    EXPECT_EQ( output.size(), table.size() - 1 );
+    const std::vector<int> expected_table = {
+        0, 3, 6, 9, 12, 15, 18, 21, 24, 27,
+        30, 33, 34, 35,  36, 39, 42
+    };
+    EXPECT_EQ( expected_table, table );
+}
+
+
+TEST_F(MISC_NormTest, halfwidth_katakana_without_voiced_sound_mark)
+{
+    std::string output;
+    std::vector<int> table;
+    constexpr const char halfwidth[] {
+        "\uFF61\uFF62\uFF63\uFF64\uFF65" "\uFF66" "\uFF67\uFF68\uFF69\uFF6A\uFF6B"
+        "\uFF6C\uFF6D\uFF6E\uFF6F\uFF70" "\uFF71\uFF72\uFF73\uFF74\uFF75" "\uFF76\uFF77\uFF78\uFF79\uFF7A"
+        "\uFF7B\uFF7C\uFF7D\uFF7E\uFF7F" "\uFF80\uFF81\uFF82\uFF83\uFF84" "\uFF85\uFF86\uFF87\uFF88\uFF89"
+        "\uFF8A\uFF8B\uFF8C\uFF8D\uFF8E" "\uFF8F\uFF90\uFF91\uFF92\uFF93" "\uFF94\uFF95\uFF96"
+        "\uFF97\uFF98\uFF99\uFF9A\uFF9B" "\uFF9C\uFF9D"
+    };
+    constexpr const char fullwidth[] {
+        "。「」、・" "ヲ" "ァィゥェォ"
+        "ャュョッー" "アイウエオ" "カキクケコ"
+        "サシスセソ" "タチツテト" "ナニヌネノ"
+        "ハヒフヘホ" "マミムメモ" "ヤユヨ"
+        "ラリルレロ" "ワン"
+    };
+
+    // 半角片仮名から全角片仮名へ一対一の変換
+    MISC::norm( halfwidth, output, &table );
+
+    EXPECT_EQ( fullwidth, output );
+    EXPECT_EQ( output.size(), table.size() - 1 );
+    for( int i = 0, size = table.size(); i < size; ++i ) {
+        EXPECT_EQ( i, table.at( i ) );
+    }
+}
+
+TEST_F(MISC_NormTest, halfwidth_katakana_only_voiced_sound_mark)
+{
+    std::string output;
+    std::vector<int> table;
+
+    // 半角の濁点と半濁点は全角結合文字に変換される
+    MISC::norm( "\uFF9E\uFF9F", output, &table );
+
+    EXPECT_EQ( "\u3099\u309A", output );
+    EXPECT_EQ( output.size(), table.size() - 1 );
+    for( int i = 0, size = table.size(); i < size; ++i ) {
+        EXPECT_EQ( i, table.at( i ) );
+    }
+}
+
+TEST_F(MISC_NormTest, halfwidth_katakana_combining_voiced_sound_mark_to_decomposed)
+{
+    std::string output;
+    std::vector<int> table;
+    constexpr const char halfwidth[] = {
+        "\uFF76\uFF9E\uFF77\uFF9E\uFF78\uFF9E\uFF79\uFF9E\uFF7A\uFF9E"
+        "\uFF7B\uFF9E\uFF7C\uFF9E\uFF7D\uFF9E\uFF7E\uFF9E\uFF7F\uFF9E"
+        "\uFF80\uFF9E\uFF81\uFF9E\uFF82\uFF9E\uFF83\uFF9E\uFF84\uFF9E"
+        "\uFF8A\uFF9E\uFF8B\uFF9E\uFF8C\uFF9E\uFF8D\uFF9E\uFF8E\uFF9E"
+        "\uFF8A\uFF9F\uFF8B\uFF9F\uFF8C\uFF9F\uFF8D\uFF9F\uFF8E\uFF9F"
+    };
+    constexpr const char fullwidth[] {
+        "\u30AB\u3099\u30AD\u3099\u30AF\u3099\u30B1\u3099\u30B3\u3099" // ガギグゲゴ
+        "\u30B5\u3099\u30B7\u3099\u30B9\u3099\u30BB\u3099\u30BD\u3099" // ザジズゼゾ
+        "\u30BF\u3099\u30C1\u3099\u30C4\u3099\u30C6\u3099\u30C8\u3099" // ダヂヅデド
+        "\u30CF\u3099\u30D2\u3099\u30D5\u3099\u30D8\u3099\u30DB\u3099" // バビブベボ
+        "\u30CF\u309A\u30D2\u309A\u30D5\u309A\u30D8\u309A\u30DB\u309A" // パピプペポ
+    };
+
+    // 合成済み文字が存在する(半)濁点付き半角片仮名は全角片仮名と結合文字へ変換される
+    MISC::norm( halfwidth, output, &table );
+
+    EXPECT_EQ( fullwidth, output );
+    EXPECT_EQ( output.size(), table.size() - 1 );
+
+    std::vector<int> expected_table( table.size() );
+    std::iota( expected_table.begin(), expected_table.end(), 0 );
+    EXPECT_EQ( expected_table, table );
+}
+
+TEST_F(MISC_NormTest, halfwidth_katakana_combining_voiced_sound_mark_wagyo)
+{
+    std::string output;
+    std::vector<int> table;
+
+    // 濁点付き半角片仮名のウやワ行は全角片仮名と結合文字へ変換される
+    MISC::norm( "\uFF73\uFF9E\uFF9C\uFF9E\uFF66\uFF9E", output, &table );
+
+    EXPECT_EQ( "\u30A6\u3099\u30EF\u3099\u30F2\u3099", output );
+    EXPECT_EQ( output.size(), table.size() - 1 );
+    std::vector<int> expected_table( table.size() );
+    std::iota( expected_table.begin(), expected_table.end(), 0 );
+    EXPECT_EQ( expected_table, table );
+}
+
+TEST_F(MISC_NormTest, halfwidth_katakana_combining_voiced_sound_mark_through)
+{
+    std::string output;
+    std::vector<int> table;
+
+    // 合成済み文字が存在しない(半)濁点付き半角片仮名は全角片仮名と結合文字に変換される : カ゚キ゚ク゚ケ゚コ゚
+    // NOTE: 組み合わせが多いのでテストは網羅していない
+    MISC::norm( "\uFF76\uFF9F\uFF77\uFF9F\uFF78\uFF9F\uFF79\uFF9F\uFF7A\uFF9F", output, &table );
+
+    EXPECT_EQ( "\u30AB\u309A\u30AD\u309A\u30AF\u309A\u30B1\u309A\u30B3\u309A", output );
+    EXPECT_EQ( output.size(), table.size() - 1 );
+    for( int i = 0, size = table.size(); i < size; ++i ) {
+        EXPECT_EQ( i, table.at( i ) );
+    }
+}
+
+
+TEST_F(MISC_NormTest, fullwidth_katakana_without_voiced_sound_mark)
+{
+    std::string output;
+    std::vector<int> table;
+    constexpr const char fullwidth[] {
+        "\u30A1\u30A2\u30A3\u30A4\u30A5\u30A6\u30A7\u30A8\u30A9\u30AA" // ァ - オ
+        "\u30AB\u30AD\u30AF\u30B1\u30B3\u30B5\u30B7\u30B9\u30BB\u30BD" // カ - ソ
+        "\u30BF\u30C1\u30C3\u30C4\u30C6\u30C8\u30CA\u30CB\u30CC\u30CD\u30CE" // タ - ノ
+        "\u30CF\u30D2\u30D5\u30D8\u30DB\u30DE\u30DF\u30E0\u30E1\u30E2" // ハ - モ
+        "\u30E3\u30E4\u30E5\u30E6\u30E7\u30E8\u30E9\u30EA\u30EB\u30EC\u30ED" // ャ - ロ
+        "\u30EE\u30EF\u30F0\u30F1\u30F2\u30F3\u30F5\u30F6" // ヮ - ヶ
+    };
+
+    // (半)濁点の付いていない全角片仮名はそのまま
+    MISC::norm( fullwidth, output, &table );
+
+    EXPECT_EQ( fullwidth, output );
+    EXPECT_EQ( output.size(), table.size() - 1 );
+    for( int i = 0, size = table.size(); i < size; ++i ) {
+        EXPECT_EQ( i, table.at( i ) );
+    }
+}
+
+TEST_F(MISC_NormTest, fullwidth_katakana_precomposed_voiced_sound_mark)
+{
+    std::string output;
+    std::vector<int> table;
+    constexpr const char fullwidth[] {
+        "\u30AC\u30AE\u30B0\u30B2\u30B4\u30B6\u30B8\u30BA\u30BC\u30BE" // ガ - ゾ
+        "\u30C0\u30C2\u30C5\u30C7\u30C9\u30D0\u30D3\u30D6\u30D9\u30DC" // ダ - ボ
+        "\u30F4\u30F7\u30F8\u30F9\u30FA" // ヴ - ヺ
+        "\u30D1\u30D4\u30D7\u30DA\u30DD" // パ - ポ
+    };
+    constexpr const char fullwidth_combining[] {
+        "\u30AB\u3099\u30AD\u3099\u30AF\u3099\u30B1\u3099\u30B3\u3099" // ガギグゲゴ
+        "\u30B5\u3099\u30B7\u3099\u30B9\u3099\u30BB\u3099\u30BD\u3099" // ザジズゼゾ
+        "\u30BF\u3099\u30C1\u3099\u30C4\u3099\u30C6\u3099\u30C8\u3099" // ダヂヅデド
+        "\u30CF\u3099\u30D2\u3099\u30D5\u3099\u30D8\u3099\u30DB\u3099" // バビブベボ
+        "\u30A6\u3099\u30EF\u3099\u30F0\u3099\u30F1\u3099\u30F2\u3099" // ヴヷヸヹヺ
+        "\u30CF\u309A\u30D2\u309A\u30D5\u309A\u30D8\u309A\u30DB\u309A" // パピプペポ
+    };
+
+    // 合成済み文字の(半)濁点付き全角片仮名は結合文字が分解される
+    MISC::norm( fullwidth, output, &table );
+
+    EXPECT_EQ( fullwidth_combining, output );
+    EXPECT_EQ( output.size(), table.size() - 1 );
+    for( std::size_t i = 0; i < table.size() - 6; i += 6 ) {
+        EXPECT_EQ( i / 2 + 0, table.at(i + 0) );
+        EXPECT_EQ( i / 2 + 1, table.at(i + 1) );
+        EXPECT_EQ( i / 2 + 2, table.at(i + 2) );
+        EXPECT_EQ( i / 2 + 3, table.at(i + 3) );
+        EXPECT_EQ( i / 2 + 4, table.at(i + 4) );
+        EXPECT_EQ( i / 2 + 5, table.at(i + 5) );
+    }
+}
+
+TEST_F(MISC_NormTest, fullwidth_katakana_combining_voiced_sound_mark)
+{
+    std::string output;
+    std::vector<int> table;
+    constexpr const char fullwidth[] {
+        "\u30AB\u3099\u30AD\u3099\u30AF\u3099\u30B1\u3099\u30B3\u3099" // ガギグゲゴ
+        "\u30AB\u309A\u30AD\u309A\u30AF\u309A\u30B1\u309A\u30B3\u309A" // カ゚キ゚ク゚ケ゚コ゚
+    };
+
+    // 合成済み文字の有無に関係なく(半)濁点の結合文字が付いている全角片仮名は変換されない
+    // NOTE: 組み合わせが多いのでテストは網羅していない
+    MISC::norm( fullwidth, output, &table );
+
+    EXPECT_EQ( fullwidth, output );
+    EXPECT_EQ( output.size(), table.size() - 1 );
+    for( int i = 0, size = table.size(); i < size; ++i ) {
+        EXPECT_EQ( i, table.at( i ) );
+    }
+}
+
+TEST_F(MISC_NormTest, fullwidth_hiragana_without_voiced_sound_mark)
+{
+    std::string output;
+    std::vector<int> table;
+    constexpr const char fullwidth[] {
+        "\u3041\u3042\u3043\u3044\u3045\u3046\u3047\u3048\u3049\u304A" // ぁ - お
+        "\u304B\u304D\u304F\u3051\u3053\u3055\u3057\u3059\u305B\u305D" // か - そ
+        "\u305F\u3061\u3063\u3064\u3066\u3068\u306A\u306B\u306C\u306D\u306E" // た - の
+        "\u306F\u3072\u3075\u3078\u307B\u307E\u307F\u3080\u3081\u3082" // は - も
+        "\u3083\u3084\u3085\u3086\u3087\u3088\u3089\u308A\u308B\u308C\u308D" // ゃ - ろ
+        "\u308E\u308F\u3090\u3091\u3092\u3093\u3095\u3096" // ゎ - ゖ
+    };
+
+    // (半)濁点の付いていない全角平仮名はそのまま
+    MISC::norm( fullwidth, output, &table );
+
+    EXPECT_EQ( fullwidth, output );
+    EXPECT_EQ( output.size(), table.size() - 1 );
+    for( int i = 0, size = table.size(); i < size; ++i ) {
+        EXPECT_EQ( i, table.at( i ) );
+    }
+}
+
+TEST_F(MISC_NormTest, fullwidth_hiragana_precomposed_voiced_sound_mark)
+{
+    std::string output;
+    std::vector<int> table;
+    constexpr const char fullwidth[] {
+        "\u304C\u304E\u3050\u3052\u3054\u3056\u3058\u305A\u305C\u305E" // が - ぞ
+        "\u3060\u3062\u3065\u3067\u3069\u3070\u3073\u3076\u3079\u307C" // だ - ぼ
+        "\u3094" // ゔ
+        "\u3071\u3074\u3077\u307A\u307D" // ぱ - ぽ
+    };
+    constexpr const char fullwidth_combining[] {
+        "\u304B\u3099\u304D\u3099\u304F\u3099\u3051\u3099\u3053\u3099"
+        "\u3055\u3099\u3057\u3099\u3059\u3099\u305B\u3099\u305D\u3099"
+        "\u305F\u3099\u3061\u3099\u3064\u3099\u3066\u3099\u3068\u3099"
+        "\u306F\u3099\u3072\u3099\u3075\u3099\u3078\u3099\u307B\u3099"
+        "\u3046\u3099"
+        "\u306F\u309A\u3072\u309A\u3075\u309A\u3078\u309A\u307B\u309A"
+    };
+
+    // 合成済み文字の(半)濁点付き全角平仮名は結合文字が分解される
+    MISC::norm( fullwidth, output, &table );
+
+    EXPECT_EQ( fullwidth_combining, output );
+    EXPECT_EQ( output.size(), table.size() - 1 );
+    for( std::size_t i = 0; i < table.size() - 6; i += 6 ) {
+        EXPECT_EQ( i / 2 + 0, table.at(i + 0) );
+        EXPECT_EQ( i / 2 + 1, table.at(i + 1) );
+        EXPECT_EQ( i / 2 + 2, table.at(i + 2) );
+        EXPECT_EQ( i / 2 + 3, table.at(i + 3) );
+        EXPECT_EQ( i / 2 + 4, table.at(i + 4) );
+        EXPECT_EQ( i / 2 + 5, table.at(i + 5) );
+    }
+}
+
+TEST_F(MISC_NormTest, fullwidth_hiragana_combining_voiced_sound_mark)
+{
+    std::string output;
+    std::vector<int> table;
+    constexpr const char fullwidth[] {
+        "\u304B\u3099\u304D\u3099\u304F\u3099\u3051\u3099\u3053\u3099" // がぎぐげご
+        "\u304B\u309A\u304D\u309A\u304F\u309A\u3051\u309A\u3053\u309A" // か゚き゚く゚け゚こ゚
+    };
+
+    // 合成済み文字の有無に関係なく(半)濁点の結合文字が付いている全角平仮名は変換されない
+    // NOTE: 組み合わせが多いのでテストは網羅していない
+    MISC::norm( fullwidth, output, &table );
 
     EXPECT_EQ( fullwidth, output );
     EXPECT_EQ( output.size(), table.size() - 1 );
