b3d1864a1408a5ca4dcde23cfc56ab9eb6f9f648
Masayuki Yamamoto <15698961+ma8ma@users.noreply.github.com>
2023-03-04 21:56:11 +0900

    Iconv: Add constructors with enum Encoding parameters (#1117)
    `Encoding`列挙型を引数に取る`JDLIB::Iconv`クラスのコンストラクターを
    追加します。
    
    Co-authored-by: JD Project <jd.project@acc574344b8506f1335297eaa7f74be0f7ea992b>

--- src/jdlib/jdiconv.h
+++ src/jdlib/jdiconv.h
@@ -3,6 +3,8 @@
 #ifndef _JDICONV_H
 #define _JDICONV_H
 
+#include "jdencoding.h"
+
 #include <string>
 #include <vector>
 
@@ -18,19 +20,24 @@ namespace JDLIB
 
         std::string m_buf; ///< 出力バッファ
 
-        std::string m_coding_from; ///< 変換元の文字エンコーディング
-        bool m_coding_to_is_utf8; ///< 変換先の文字エンコーディングがUTF-8ならtrue
+        Encoding m_enc_to;   ///< 変換先の文字エンコーディング
+        Encoding m_enc_from; ///< 変換元の文字エンコーディング
         bool m_broken_sjis_be_utf8; ///< trueなら不正なMS932文字列をUTF-8と見なす (MS932 -> UTF-8の変換限定)
 
     public:
 
         Iconv( const std::string& coding_to, const std::string& coding_from );
         Iconv( const std::string& coding_to, const std::string& coding_from, const bool broken_sjis_be_utf8 );
+        Iconv( const Encoding to, const Encoding from );
+        Iconv( const Encoding to, const Encoding from, const bool broken_sjis_be_utf8 );
         ~Iconv();
 
         // テキストの文字エンコーディングを変換する
         const std::string& convert( char* str_in, std::size_t size_in );
         std::string& convert( char* str_in, std::size_t size_in, std::string& out_buf );
+
+    private:
+        void open_by_alternative_names( const char* to_str, const char* from_str );
     };
 }
 
